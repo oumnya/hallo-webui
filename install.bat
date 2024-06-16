af02e3857b425c1910d58dce68a2ec00a57e1b2c
Danil Boldyrev <daswerq123@gmail.com>
2024-06-16 03:35:07 +0300

    fix

--- install.bat
+++ install.bat
@@ -15,7 +15,6 @@ pip install bitsandbytes-windows --force-reinstall
 
 echo Install GPU libs
 pip install torch==2.2.2+cu121 torchaudio torchvision --index-url https://download.pytorch.org/whl/cu121
-pip install onnxruntime-gpu
 
 echo install complete
 pause
